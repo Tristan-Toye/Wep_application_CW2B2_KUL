685eb1d69794f4ed68cc3f529f351df15b822929
Tristan-Toye <92030451+Tristan-Toye@users.noreply.github.com>
2021-12-13 18:46:22 +0100

    Update constants.py

--- flask_info/constants.py
+++ flask_info/constants.py
@@ -1,6 +1,6 @@
 from flask import url_for
 
-PRE_URL_STRING = 'http://127.0.0.1:5000'
+PRE_URL_STRING = 'https://identigate.herokuapp.com/'
 secret_key_pyotp = 'JD2E34VPNRYGWID3O7LDVSUTM6R66MUX'
 max_attempts_google_auth=3
 min_attempts_password_before_notification = 2
